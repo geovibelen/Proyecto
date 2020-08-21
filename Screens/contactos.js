da0b1d79f2b11572f37ec1501bd1a1d4a62b590e
geovibelen <geovibelen2@hotmail.com>
2020-08-20 19:04:00 -0500

    Creacion carpeta Screen

---
+++ Screens/contactos.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+export class Contactos extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LISTA DE CONTACTOS</Text>
+      </View>
+    );
+  }
+}
